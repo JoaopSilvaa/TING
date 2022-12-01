82e261347da1cc8e245fe80594b1c64840dbc5fa
Joao Antonio <joaopsilva.dev@gmail.com>
2022-12-01 19:16:03 -0300

    feat: :sparkles: requisito 7 completo
    função search_by_word completa com refatoração da exists_word para ficar mais interessante

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -9,15 +9,29 @@ def verify_occurrences(result):
     return occurrences_empty
 
 
-def exists_word(word: str, instance: Queue):
+def iterator_in_instance_with_content(word, data):
+    lines = data["linhas_do_arquivo"]
+    occurrences = []
+    for j in range(0, len(lines)):
+        if word.lower() in lines[j].lower():
+            occurrences.append({"linha": (j + 1), "conteudo": lines[j]})
+    return occurrences
+
+
+def iterator_in_instance_without_content(word, data):
+    lines = data["linhas_do_arquivo"]
+    occurrences = []
+    for j in range(0, len(lines)):
+        if word.lower() in lines[j].lower():
+            occurrences.append({"linha": (j + 1)})
+    return occurrences
+
+
+def verify_generics(word, instance, type_verify):
     result = []
     for i in range(0, len(instance)):
         data = instance.search(i)
-        lines = data["linhas_do_arquivo"]
-        occurrences = []
-        for j in range(0, len(lines)):
-            if word.lower() in lines[j].lower():
-                occurrences.append({"linha": (j + 1)})
+        occurrences = type_verify(word, data)
         content = {
             "palavra": word.lower(),
             "arquivo": data["nome_do_arquivo"],
@@ -30,5 +44,15 @@ def exists_word(word: str, instance: Queue):
     return result if verify == 0 else []
 
 
+def exists_word(word: str, instance: Queue):
+    return verify_generics(
+        word,
+        instance,
+        iterator_in_instance_without_content)
+
+
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return verify_generics(
+        word,
+        instance,
+        iterator_in_instance_with_content)

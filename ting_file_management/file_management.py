aedb72a91ae6f693391c2ebb03351572d29d84e3
Joao Antonio <joaopsilva.dev@gmail.com>
2022-12-01 17:21:26 -0300

    feat: :sparkles: requisito 2 completo
    função txt_importer completa, importando arquivos txt e devolvendo seu conteúdo

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if ".txt" not in path_file:
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, mode="r") as file:
+            content = [line for line in file.read().split('\n')]
+        return content
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
